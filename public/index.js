7789db1c9d1ea0d95fa883d85224ad0b9fa74729
Madhav Maheshwari <maheshwarimadhav1234@gmail.com>
2024-01-25 18:59:34 +0530

    CSS Added

--- public/index.js
+++ public/index.js
@@ -15,11 +15,13 @@ mongoose.connect('mongodb://127.0.0.1:27017/ZanskarSports',{useNewUrlParser: tru
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
+app.use(express.static(path.join(__dirname,'views')));
+
 app.get('/matches',async(req,res)=>{
     const matches = await Match.find({})
     console.log(matches)
-    // res.render('index',{matches})
-    res.send('HERE')
+    res.render('matches/index',{matches})
+    //res.send('HERE')
 })
 
 app.listen(3000,()=>{
